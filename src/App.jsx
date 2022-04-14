3b691a8e7c8e6870b64588fffaf7902ec73ad3a2
wangqt <969909031@qq.com>
2022-04-14 23:21:07 +0800

    eslint 配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
 import "./style.scss";
-
-
+import One from './pages/One';
 
 export default function App() {
   return (
     <div className='box'>
       <span>box</span>
+      <One></One>
     </div>
   )
 }
